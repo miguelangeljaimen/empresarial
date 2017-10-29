64e4c920beb449f4c65b96b4c78845165084288d
miguelangel <miguelangel.jaimen@gmail.com>
2017-10-29 19:10:24 -0300

    problemas entre negocio Dao y Modelos 2

---
+++ src/java/cl/solemne/dao/CombosDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.solemne.dao;
+import cl.solemne.modelos.Combos;
+/**
+ *
+ * @author miguelangel
+ */
+public class CombosDao extends Dao<cl.solemne.modelos.Combos>{
+    
+    public CombosDao() {
+        super(Combos.class);
+    }
+    
+}
---
+++ src/java/cl/solemne/dao/ProveedorDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.solemne.dao;
+import cl.solemne.modelos.Proveedor;
+/**
+ *
+ * @author miguelangel
+ */
+public class ProveedorDao extends Dao<Proveedor>{
+    
+    public ProveedorDao() {
+        super(Proveedor.class);
+    }
+    
+}
+
+
